8dcd37ec2c64dd09bf564812b144d6d3351e69bc
davon09 <139665217+davon09@users.noreply.github.com>
2024-10-11 20:20:37 +0800

    Update get_rides.php

--- get_rides.php
+++ get_rides.php
@@ -1,12 +1,10 @@
 <?php
-// Simulated data for available rides
 $rides = [
     ['route' => 'Bakakeng to City', 'time' => '8:00 AM', 'seats' => 12],
     ['route' => 'City to Bakakeng', 'time' => '10:00 AM', 'seats' => 8],
     ['route' => 'Bakakeng to City', 'time' => '2:00 PM', 'seats' => 15],
 ];
 
-// Send JSON response
 header('Content-Type: application/json');
 echo json_encode($rides);
 ?>
