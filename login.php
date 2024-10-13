1d4fd260da3c27822ede96af7351970434cc5531
davon09 <139665217+davon09@users.noreply.github.com>
2024-10-13 11:55:20 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,27 @@
 <?php
 session_start();
-session_destroy(); 
-header('Location: login.html'); 
+
+$users = [
+    ['username' => 'student', 'password' => 'pass123'],
+    ['username' => 'faculty', 'password' => 'pass456'],
+    ['username' => 'employee', 'password' => 'pass789'],
+];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$valid = false;
+foreach ($users as $user) {
+    if ($user['username'] == $username && $user['password'] == $password) {
+        $valid = true;
+        break;
+    }
+}
+
+if ($valid) {
+    $_SESSION['username'] = $username;
+    header('Location: index.html');
+} else {
+    header('Location: login.html?error=1');
+}
 ?>
