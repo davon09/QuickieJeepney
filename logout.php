e2c6ad5605763c3b9b4038a198d6c083fae09c40
davon09 <139665217+davon09@users.noreply.github.com>
2024-10-13 11:55:33 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-session_destroy(); // Destroy the session
-header('Location: login.html'); // Redirect to login page
+session_destroy(); 
+header('Location: login.html'); 
 ?>
