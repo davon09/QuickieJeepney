d111a4be8d44649ca94f3f9f4c755d2d357147b4
Shawn <2222099@slu.edu.ph>
2024-10-23 21:56:27 +0800

    add confirm booking.php

---
+++ user/booking/confirm_booking.php
@@ -0,0 +1,18 @@
+<?php
+include '../../dbConnection/dbConnection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeepney Booking</title>
+    <link rel="stylesheet" href="confirm_booking.css">
+</head>
+<body>
+    <header class="header-bar"></header>
+
+    <div class="image-container">
+    <img src="../../images/success.png" class="image_check" alt="success">
+    <p class="text-success">Booked Successfully!</p>
+</div>
